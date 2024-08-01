a954bb793dcb9dec41ee2fa2cca040cfcdb23899
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-08-01 15:53:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+let element = document.querySelector("#level")
+level = 0;
+currentElement = element
+
+while(currentElement){
+	  level++
+	 currentElement = currentElement.parentElement
+}
+
+alert(`The level of the element is: ${level}`)
